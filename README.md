9acaaecb07467c23692c5b44d4695ad4fb7a4f3d
gustavs-porietis-rtu-231RDB328 <gustavs.porietis@edu.rtu.lv>
2024-01-18 19:28:11 +0200

    List creation of all advertisment links

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # SS.lv "skreiperis"
 
-SS.lv ir Latvijā populārākais sludinājumu portāls, taču, tas diemžēl nav mainījies jau vairākus gadus un portāla veidotāji nav implementējuši funkcijas, kas noderētu meklējot kādu preci, īpašumu vai pakalpojomu. Šī projekta mērķis ir atsūtīt lietotājam epastu, ja ir ievietots kāds jauns sludinājums izvēlētā kategorijā un ar izvēlētajiem filtriem, kā arī saglabāt visus esošos sludinājumus, lai pēc tam varētu salīdzināt cenas.
+SS.lv ir Latvijā populārākais sludinājumu portāls, taču, tas diemžēl nav mainījies jau vairākus gadus un portāla veidotāji nav implementējuši funkcijas, kas noderētu meklējot kādu preci, īpašumu vai pakalpojomu. Šī projekta mērķis ir atsūtīt lietotājam epastu, ja ir ievietots kāds jauns sludinājums izvēlētā kategorijā un ar izvēlētajā cenu robežās, kā arī saglabāt esošos sludinājumus, lai pēc tam varētu salīdzināt cenas.
 
 ## Kā lietot
 
-1. Atveriet SS.lv un izvēlaties kādu preču/pakalpojumu kategoriju (piem. Mājas, vasarnīcas -> Rīgas rajons -> Sigulda vai Viglās automašīnas -> Volvo)
-2. Izvēlaties darījuma veidu (piem. Pārdod, Izīrē)
-! Ja meklējat automašīnu, izvēlaties arī modeli
+1. Atveriet SS.lv un izvēlaties kādu preču/pakalpojumu kategoriju (piem. Mājas, vasarnīcas -> Rīgas rajons -> Sigulda vai Vieglās automašīnas -> Volvo)
+2. Izvēlaties darījuma veidu (piem. Pārdod, Izīrē) **! Ja meklējiet vieglo automašīnu, izvēlaties arī modeli**
 3. Nokopējiet URL, palaižiet main.py un ievietojiet nokopēto saiti
 4. Ievadie max un min cenu
 5. Ievadiet e-pastu (ja vēlaties, lai atnāk paziņojumi uz e-pastu par jaunu sludinajumu)
--- main.py
+++ main.py
@@ -2,16 +2,46 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 import time
-url = input("URL: ")
+input_url = input("URL: ")
 price_min = input("Min price: ")
 price_max = input("Max price: ")
 
+page = 1
+listings = []
+
 driver = webdriver.Firefox()
-driver.get(url)
+driver.get(input_url)
+try:
+    price_min_field = driver.find_element(By.ID, "f_o_8_min")
+    price_max_field = driver.find_element(By.ID, "f_o_8_max")
+    price_min_field.send_keys(price_min)
+    price_max_field.send_keys(price_max)
+    price_max_field.submit()
+except:
+    pass
+
 time.sleep(2)
+url = driver.current_url
 
-price_min_field = driver.find_element(By.ID, "f_o_8_min")
-price_max_field = driver.find_element(By.ID, "f_o_8_max")
-price_min_field.send_keys(price_min)
-price_max_field.send_keys(price_max)
-
+while True:
+    driver.get(str(url + "page" + str(page) + ".html"))
+    if "Sludinājumi dotajā kategorijā nav atrasti" in driver.page_source:
+        break
+    else:
+        time.sleep(2)
+        table_heading = driver.find_element(By.ID, "head_line")
+        next_listing = table_heading.find_element(By.XPATH, "following-sibling::*")
+        while True:
+            try:
+                next_listing = next_listing.find_element(By.XPATH, "following-sibling::*")
+            except:
+                break
+            if next_listing.get_attribute("id") == "tr_bnr_712":
+                break
+            ad_id = next_listing.get_attribute("id")[3:]
+            ad_url = next_listing.find_element(By.ID, str("dm_" + ad_id)).get_attribute("href")
+            listings.append(ad_url)
+        page = page + 1
+print(listings)
+input("Finished")
+driver.quit()
